c629557c16895842b99f05966f84a4e9b000a727
redthing1 <redthing1@alt.icu>
2024-09-01 23:47:28 -0700

    initial commit

--- __init__.py
+++ __init__.py
@@ -1,7 +1,4 @@
 from binaryninja import *
+from .gbarom import GBAView
 
-def do_nothing(bv):
-	show_message_box("Do Nothing", "Congratulations! You have successfully done nothing.\n\n" +
-					 "Pat yourself on the back.", MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.ErrorIcon)
-
-PluginCommand.register("Useless Plugin", "Basically does nothing", do_nothing)
+GBAView.register()
